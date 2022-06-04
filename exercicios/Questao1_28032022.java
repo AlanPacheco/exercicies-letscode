c3e5f0386e04d9760aeb705245c9bd14dfa64f59
Alan Pacheco <alan.estudo@gmail.com>
2022-06-04 14:55:24 -0300

    Questao1_28032022

---
+++ exercicios/Questao1_28032022.java
@@ -0,0 +1,18 @@
+public class Questao1_28032022 {
+/*    1 - Responda qual tipo de objeto deve ser usado para armazenar cada uma das seguintes informações:
+
+    a. A idade de uma pessoa.
+    b. A área de um quintal em metros quadrados.
+    c. A média da quantidade de chuva no mês de fevereiro.
+    d. O número de estrelas na galáxia.
+
+-----------------------------------------------------------
+    Respostas:
+
+    a) int
+    b) float ou double
+    c) float ou double
+    d) int
+    */
+
+}
